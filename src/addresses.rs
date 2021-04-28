e75d96b7f58608a7705cb2528f67bf7e4bd0952b
Igor Katson <igor.katson@gmail.com>
2021-04-28 17:10:01 +0100

    Addresses - nicer debug

--- src/addresses.rs
+++ src/addresses.rs
@@ -1,6 +1,6 @@
 use vm_memory::GuestAddress;
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone)]
 pub struct GuestIpaAddress(pub u64);
 impl GuestIpaAddress {
     pub const fn add(&self, offset: Offset) -> GuestIpaAddress {
@@ -11,7 +11,13 @@ impl GuestIpaAddress {
     }
 }
 
-#[derive(Copy, Clone, Default, Debug)]
+impl core::fmt::Debug for GuestIpaAddress {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("IPA({:#x?})", self.0))
+    }
+}
+
+#[derive(Copy, Clone, Default)]
 pub struct GuestVaAddress(pub u64);
 impl GuestVaAddress {
     pub const fn add(&self, offset: Offset) -> GuestVaAddress {
@@ -23,11 +29,23 @@ impl GuestVaAddress {
     }
 }
 
-#[derive(Copy, Clone, Debug)]
+impl core::fmt::Debug for GuestVaAddress {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("VA({:#x?})", self.0))
+    }
+}
+
+#[derive(Copy, Clone)]
 pub struct Offset(pub u64);
 impl Offset {
     pub const fn add(&self, offset: Offset) -> Offset {
         Offset(self.0 + offset.0)
+    }
+}
+
+impl core::fmt::Debug for Offset {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("Offset({:#x?})", self.0))
     }
 }
 
